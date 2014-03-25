5c2812423bd2dac1cfa6547986f352a1b6032eda
Clark Hinchcliff <clarkhinchcliff@hotmail.com>
2014-03-24 20:55:25 -0600

    two RB exercises done
    Finished two assignments, pushing to let others comment.

--- 1_calculate_array_total/my_solution.rb
+++ 1_calculate_array_total/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
@@ -12,14 +12,60 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+#Create a 'total' method that takes the argument 'array'
+#Have the total method return the sum of the array's numbers
+#First create the method, put the name of the array.
+#call an inject method on the function
+
+#Create sentence_maker method that takes the argument array
+#call a map method in the function and have it join each of the elements with a space in between and a period at the end.
 
 # 2. Initial Solution
 
+def total(array)
+	array.inject(0) {|sum, i| sum + i}
+end
 
-
+def sentence_maker(array)
+	sentence = array.join(" ")
+	return (sentence + ".").capitalize
+end
 
 # 3. Refactored Solution
 
+def total(array)
+	array.inject(0) {|sum, i| sum + i}
+end
 
+def sentence_maker(array)
+	sentence = array.join(" ")
+	return (sentence + ".").capitalize
+end
 
 # 4. Reflection 
+
+=begin
+	
+What parts of your strategy worked? What problems did you face?
+	I initially tried using the map function in ruby, I got confused as to how I was going to add a period or capitalize the front of the joined array.
+	I then noticed the word "joined" and just used the join function. Everything worked out fine.
+
+What questions did you have while coding? What resources did you find to help you answer them?
+	I had a few basic syntax questions, but I have a few friends in the program that helped me out.
+
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+	I'm not having problems, I'm just trying to remember everything. This is all a review.
+
+Did you learn any new skills or tricks?
+	I haven't at this activity. More of a refresher. Javascript and ruby are two different languages and I need to stay with one syntax.
+
+How confident are you with each of the learning objectives?
+	I am very confident. Let's ask me questions! Confuse me, make me learn. Help me at the same time.
+
+Which parts of the challenge did you enjoy?
+	This was nice to start of very basic to return to the ruby roots.
+
+Which parts of the challenge did you find tedious?
+	Nothing so far, I am enjoying myself.
+
+=end
--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -12,14 +12,94 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+=begin
+
+Create a method 'get_grade' with an argument of 'ary'
+Array can only accept a score between 0 and 100, null if otherwise
+Add total of array then divide by the number of objects in the array
+Average out the score and return the letter grade as a string
+90 and up for A
+80 and up for B
+70 and up for C
+60 and up for D
+59 and below for F
+
+=end
 
 # 2. Initial Solution
 
-
+def get_grade(array)
+  sum = 0
+  grade = array.inject(0) {|sum, i| sum + i}.to_f / array.length
+  if grade >= 101
+        return nil
+  end
+  if grade >= 90
+  	return "A"
+  end
+  if grade >= 80
+      return "B"
+  end
+  if grade >= 70
+      return "C"
+  end
+  if grade >= 60
+      return "D"
+  end
+  if grade <= -1
+    return nil
+	end
+  if grade <= 50
+    return "F"
+  end
+end
 
 
 # 3. Refactored Solution
 
+def get_grade(array)
+  sum = 0
+  grade = array.inject(0) {|sum, i| sum + i}.to_f / array.length
+  if grade >= 90
+  	return "A"
+  elsif grade >= 80
+      return "B"
+  elsif grade >= 70
+      return "C"
+  elsif grade >= 60
+      return "D"
+	else
+		return "F"
+	end
+end
+
+#4 Reflection
+
+=begin
+What parts of your strategy worked? What problems did you face?
+I had put in several ends with each of my elsif statements and was wondering why they weren't working
+Then I discovered that there was only need for one end. That make refactoring my code a loooot easier.
+
+What questions did you have while coding? What resources did you find to help you answer them?
+I was wondering what I would do if I wanted to make the grade nil if it was negative or greater than 100%.
+	I just assume that I would use elsif statements and make the loop return nil.
+
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+	I struggled initially to get back into the ruby groove, but I think I've got it now.
 
 
-# 4. Reflection 
+Did you learn any new skills or tricks?
+	.to_f will make it a float if we get any decimal grades.
+
+
+How confident are you with each of the learning objectives?
+	Getting pretty confident.
+
+
+Which parts of the challenge did you enjoy?
+	I really like how this took what we did on socrates and mixed it up with a mean function.
+
+Which parts of the challenge did you find tedious?
+	I'm honestly having a blast doing all of this coding.
+	
+=end
